2f348933c414b05b4a0c5e16474e9cd209d77584
ggg50 <xjfletitgo@gmail.com>
2019-09-08 00:53:20 +0800

    chore: 上传 yapi 脚本代码

---
+++ yapi脚本.js
@@ -0,0 +1,262 @@
+let data = {
+  payload: {
+    content: [
+      {
+        id: '1',
+        name: 'todoList',
+        type: '前端组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1566543026678
+      },
+      {
+        id: '2',
+        name: 'eTest组件',
+        type: '测试',
+        version: '1.0.1',
+        language: 'javascript',
+        status: '下架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1566509103484
+      },
+      {
+        id: '3',
+        name: 'test2',
+        type: '应用服务',
+        version: '1.0.3',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1566509103484
+      },
+      {
+        id: '4',
+        name: 'test3',
+        type: '数据存储',
+        version: '1.2.0',
+        language: 'javascript',
+        status: '下架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1566091187149
+      },
+      {
+        id: '5',
+        name: 'test4',
+        type: '分布式工具',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '下架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1566091187149
+      },
+      {
+        id: '6',
+        name: 'test5',
+        type: '前端组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1565879600068
+      },
+      {
+        id: '7',
+        name: 'test5',
+        type: '前端组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '下架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1565879600068
+      },
+      {
+        id: '8',
+        name: 'test6',
+        type: '分布式工具',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1565879600068
+      },
+      {
+        id: '9',
+        name: 'test7',
+        type: '测试代码',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1565528373445
+      },
+      {
+        id: '10',
+        name: 'test8',
+        type: '应用服务',
+        version: '2.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1565528373445
+      },
+      {
+        id: '11',
+        name: 'test9',
+        type: '前端组件',
+        version: '1.1.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1565528373445
+      },
+      {
+        id: '12',
+        name: 'test10',
+        type: '组件test',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564824798269
+      },
+      {
+        id: '13',
+        name: 'test11',
+        type: '前端组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564824798269
+      },
+      {
+        id: '14',
+        name: 'test12',
+        type: '前端组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564824798269
+      },
+      {
+        id: '15',
+        name: 'test13',
+        type: '分布式组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564557278562
+      },
+      {
+        id: '16',
+        name: 'test14',
+        type: '测试代码',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564376146782
+      },
+      {
+        id: '17',
+        name: 'test15',
+        type: '前端组件',
+        version: '3.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564346022430
+      },
+      {
+        id: '18',
+        name: 'test16',
+        type: '数据存储',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1564214533971
+      },
+      {
+        id: '19',
+        name: 'test17',
+        type: '前端组件',
+        version: '2.0.4',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1563979753413
+      },
+      {
+        id: '20',
+        name: 'test18',
+        type: ' 数据存储',
+        version: '3.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1563905231375
+      },
+      {
+        id: '21',
+        name: 'test',
+        type: '前端组件',
+        version: '1.0.0',
+        language: 'javascript',
+        status: '上架',
+        tenantCode: '61fb5ab198064d66ad417d387fcb7fe9',
+        lastUpdateTime: 1563560874126
+      }
+    ],
+    first: true,
+    last: true,
+    number: 0,
+    numberOfElements: 3,
+    size: 10,
+    totalElements: 21,
+    totalPages: 3
+  }
+}
+
+let deletedItems = []
+if (params.deletedItems) {
+  deletedItems = JSON.parse(params.deletedItems)
+}
+
+if (params.type || params.name || params.status) {
+  let filtedItems = data.payload.content
+  if (params.type) {
+    filtedItems = filtedItems.filter(item => item.type === params.type)
+  }
+  if (params.name) {
+    filtedItems = filtedItems.filter(
+      item => item.name.indexOf(params.name) > -1
+    )
+  }
+  if (params.status) {
+    filtedItems = filtedItems.filter(item => item.status === params.status)
+  }
+  data.payload.content = filtedItems
+}
+
+let sendContent = []
+let index = 1
+for (let i = 0; i < data.payload.content.length; i++) {
+  if (deletedItems.indexOf(data.payload.content[i].id) > -1) {
+    continue
+  }
+  data.payload.content[i].index = index
+  sendContent.push(data.payload.content[i])
+  index += 1
+  if (sendContent.length >= 10) {
+    break
+  }
+}
+
+data.payload.content = sendContent
+
+mockJson = data
